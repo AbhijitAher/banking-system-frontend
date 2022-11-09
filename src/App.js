0a1fe007ce1913746e8627ca35dcfd45792fd4a6
Abhijit <abhijit.aher@freshvnf.com>
2022-11-09 18:33:35 +0530

    checked first request

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import './App.css';
+import './App.css'
+import { useEffect } from 'react'
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+  useEffect(() => {
+    getAllUsers()
+  }, [])
+
+  const getAllUsers = () => {
+    fetch('http://localhost:3030/user')
+      .then(async (res) => {
+        let users = await res.json()
+        console.log(users)
+      })
+      .catch((e) => {
+        console.log(e)
+      })
+  }
+
+  return <div className="App"></div>
 }
 
-export default App;
+export default App
